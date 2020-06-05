9af3233bcbd9753ecac3ead9c7ff3c0fb4950246
Raphael Devs <raphaeldeoliveiracorrea@gmail.com>
2020-06-05 00:39:38 -0300

    Criação do handleClickController

--- js/index.js
+++ js/index.js
@@ -6,3 +6,13 @@ const Game = {
   currentLevel: 0,
   record: 0
 }
+
+const buttons = document.querySelectorAll(".button");
+
+for (const button of buttons) {
+  button.addEventListener('click', handleClickController)
+}
+
+function handleClickController(event) {
+  console.log(event.target.dataset.color)
+}
