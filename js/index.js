bd1e35865476def8dcbf93bad304045fff9d15e5
Raphael Devs <raphaeldeoliveiracorrea@gmail.com>
2020-06-05 01:10:53 -0300

    Inserindo lógica para reproduzir sons

--- js/index.js
+++ js/index.js
@@ -1,12 +1,34 @@
 const Game = {
   canStart: true,
   started: false,
+  start: () => {
+    createClickListeners()
+
+    this.canStart = false
+    this.started = true
+  },
+  over: () => {
+    this.canStart = true
+    this.started = false
+
+    this.animation.gameOver()
+    this.playSong.gameOver()
+  },
   sequence: [],
   currentSequence: [],
   currentLevel: 0,
-  record: 0
+  record: 0,
+  animations: {
+    gameOver: () => {},
+    bip: () => {},
+  },
+  playSong: {
+    button: color => new Audio(`../sounds/${color}.mp3`).play(),
+    gameOver: () => {}
+  }
 }
 
+// Click Controller
 const buttons = document.querySelectorAll(".button");
 
 for (const button of buttons) {
@@ -16,3 +38,16 @@ for (const button of buttons) {
 function handleClickController(event) {
   console.log(event.target.dataset.color)
 }
+
+
+// key pressed controller
+document.addEventListener('keypress', handleKeyPressController)
+
+function handleKeyPressController(event) {
+  console.log(event.key)
+}
+
+
+//game.start => clickListeners, set variables
+
+// click listener => check click => function to add sequence
